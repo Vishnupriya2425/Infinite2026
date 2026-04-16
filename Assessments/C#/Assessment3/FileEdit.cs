fd47184f4ea178a6ffb92191a6100658870b24bc
Vishnupriya Arangarajan <vishnupriyaa@infinite.com>
2026-04-16 10:56:20 +0530

    Assessment

---
+++ Assessments/C#/Assessment3/FileEdit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment3
+{
+    public class FileEdit
+    {
+        public static void Main (string[] args)
+        {
+            string filePath = "demo.txt";
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+                Console.WriteLine("The file has been created successfully!!!...");
+            }
+            Console.WriteLine("Enter the text to write to the file:");
+            string text= Console.ReadLine();
+            using(StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(text);
+            }
+            Console.WriteLine("The text has been written to the according file successfully!!!...");
+        }
+    }
+}
---
+++ Assessments/C#/Assessment3/MobilePhone.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment3
+{
+    public class MobilePhone
+    {
+        public delegate void RingEventHandler();
+        public event RingEventHandler OnRing;
+        public void ReceiveCall()
+        {
+            Console.WriteLine("Incoming phone call!!...");
+            if (OnRing != null)
+            {
+                OnRing();
+            }
+        }
+    }
+    class RingTonePlayer
+    {
+        public void PlayTone()
+        {
+            Console.WriteLine("PLaying the ringtone!!!...");
+
+        }
+    }
+    class ScreenDisplay
+    {
+        public void ShowCallerInformation()
+        {
+            Console.WriteLine("Displaying caller information on the screen!!!...");
+        }
+    }
+    class vibrationMotor
+    {
+        public void Vibrate()
+        {
+            Console.WriteLine("The mobile phone is vibrating!!!...");
+        }
+    }
+
+    class PhoneRing
+    {
+        static void Main(string[] args)
+        {
+            MobilePhone phone = new MobilePhone();
+            RingTonePlayer ringTonePlayer = new RingTonePlayer();
+            ScreenDisplay screenDisplay = new ScreenDisplay();
+            vibrationMotor vibrationMotor = new vibrationMotor();
+            phone.OnRing += ringTonePlayer.PlayTone;
+            phone.OnRing += screenDisplay.ShowCallerInformation;
+            phone.OnRing += vibrationMotor.Vibrate;
+            phone.ReceiveCall();
+        }
+
+    }
+}
+
